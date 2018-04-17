e9dc023e8f818684143b10c8b4468962a462a5a2
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-04-17 16:11:07 +0900

    Implemented the addEdge method of the Graph class.
    Co-authored-by: Mike Donnelly <sqldonnelly@gmail.com>
    Co-authored-by: Takahiro Morita <morita657@gmail.com>

--- src/Graph.js
+++ src/Graph.js
@@ -6,6 +6,21 @@ class Graph {
   addNode(value) {
     this.nodes[value] = [];
   }
+
+  removeNode(value) {
+    delete this.nodes[value];
+    for (let node in this.nodes) {
+      if (this.nodes[node].includes(value)) {
+        const index = this.nodes[node].indexOf(value);
+        this.nodes[node].splice(index, 1);
+      }
+    }
+  }
+
+  addEdge(value1, value2) {
+    this.nodes[value1].push(value2);
+    this.nodes[value2].push(value1);
+  }
 }
 
 /*
