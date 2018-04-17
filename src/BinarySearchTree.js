04f661fffd1f9c3b2acf2da45f2b2e304e150b7f
Code Chrysalis <codechrysalis@Mac-mini-2.local>
2018-04-17 14:30:13 +0900

    Updated the contains method of BinarySearchTree to pass tests
    Co-authored-by: Mike Donnelly <sqldonnelly@gmail.com>
    Co-authored-by: Takahiro Morita <morita657@gmail.com>

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,5 +1,53 @@
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 
+  insert(newValue) {
+    if (newValue <= this.value) {
+      if (!this.left) {
+        this.left = new BinarySearchTree(newValue);
+        return this.left;
+      } else {
+        this.left.insert(newValue);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BinarySearchTree(newValue);
+        return this.right;
+      } else {
+        this.right.insert(newValue);
+      }
+    }
+  }
+  contains(value) {
+    let result = false;
+    const search = (target) => {
+      if (target.value === value) {
+        result = true;
+        return result;
+      } else {
+        if (value === target.left.value || value === target.right) {
+          result = true;
+          return result;
+        } else if (value < target.left.value) {
+          search(target.left);
+        } else if (value > target.right.value) {
+          search(target.right);
+        } else {
+          result = false;
+          return result;
+        }
+      }
+    };
+    search(this);
+    return result;
+  }
+}
+
+module.exports = BinarySearchTree;
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 |X                               X
